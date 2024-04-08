12ffb84351bb7cc96f44047e5e233055280f1285
Cesar Gimeno Castellote <cesargicas@gmail.com>
2024-04-08 19:03:45 +0200

    catalogo casi terminado

--- frontend/app/src/main/java/com/example/smarttrade/BrowseProducts.kt
+++ frontend/app/src/main/java/com/example/smarttrade/BrowseProducts.kt
@@ -1,37 +1,72 @@
 package com.example.smarttrade
 
+import android.app.appsearch.AppSearchManager.SearchContext
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import android.widget.GridLayout
 import android.widget.GridView
 import android.widget.HorizontalScrollView
-import android.widget.SearchView
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.smarttrade.nonactivityclasses.LeafColor
+import com.example.smarttrade.nonactivityclasses.category_representation
 import com.example.smarttrade.nonactivityclasses.product_representation
+import com.example.smarttrade.nonactivityclasses.search_representation
+import kotlin.properties.Delegates
 
 class BrowseProducts : AppCompatActivity() {
 
+    private lateinit var searchBar:EditText
     private lateinit var adapterP: ProductAdapter
+    private lateinit var adapterS: SearchAdapter
     private lateinit var adapterCat: CategoryAdapter
+    private lateinit var rectRecommended: Rect
     private var productsShown: MutableList<product_representation> = mutableListOf()
-    private var categoriesShown: MutableList<product_representation> = mutableListOf()
+    private var categoriesShown: MutableList<category_representation> = mutableListOf()
+    private var prevSearchesShown: MutableList<search_representation> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        browseProducts = this
+
+        categoriesShown.add(category_representation("toys",R.drawable.icon_toy))
+        categoriesShown.add(category_representation("food",R.drawable.icon_food))
+        categoriesShown.add(category_representation("technology",R.drawable.icon_phone))
+        categoriesShown.add(category_representation("clothes",R.drawable.icon_shirt))
+
+        //Temporal
+        prevSearchesShown.add(search_representation("search de prueba"))
+
+        //TODO add all elements to prevSearchShown
 
         enableEdgeToEdge()
         setContentView(R.layout.activity_browse_products)
 
-        val catLayout = findViewById<HorizontalScrollView>(R.id.HorizontalScrollViewCat)
-        adapterCat = CategoryAdapter(mutableListOf())
+        searchBar = findViewById<EditText>(R.id.SearchBar)
+
+        val searchLayout = findViewById<RecyclerView>(R.id.recyclerViewSearches)
+        adapterS = SearchAdapter(prevSearchesShown)
+
+        val catLayout = findViewById<RecyclerView>(R.id.HorizontalScrollViewCat)
+        adapterCat = CategoryAdapter(categoriesShown)
 
         val gridLayout = findViewById<GridView>(R.id.gridViewProd)
         adapterP = ProductAdapter(mutableListOf())
 
-
+        searchLayout.adapter = adapterS
+        searchLayout.layoutManager = LinearLayoutManager(this)
+        catLayout.adapter = adapterCat
+        catLayout.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         gridLayout.adapter = adapterP
 
         //TODO conseguir los datos de la BD y meterlos a la lista popularProducts
@@ -39,12 +74,62 @@ class BrowseProducts : AppCompatActivity() {
         for(i in 1..30){
             productsShown.add(product_representation("","","",1,LeafColor.GREEN,33))
         }
+
         adapterP.addAllProducts(productsShown)
 
-        val searchBar = findViewById<EditText>(R.id.SearchBar)
-        searchBar.setOnClickListener{
+        val topLayout = findViewById<ConstraintLayout>(R.id.constraintLayoutTop)
+        val recommendationLayout = findViewById<ConstraintLayout>(R.id.recomendationsLayout)
+        topLayout.bringChildToFront(recommendationLayout)
+        recommendationLayout.visibility = View.GONE
+
+        val startSearch = findViewById<ImageView>(R.id.imageViewSearchButton)
+        startSearch.setOnClickListener{
             //TODO search method
         }
 
+        searchBar.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus){
+                recommendationLayout.visibility = View.VISIBLE
+                searchBar.background = AppCompatResources.getDrawable(this,R.drawable.searchbar_background_desplegado)
+            } else {
+                recommendationLayout.visibility = View.GONE
+                searchBar.background = AppCompatResources.getDrawable(this,R.drawable.rounded_button_cancel)
+            }
+        }
+        rectRecommended = Rect()
+        recommendationLayout.getGlobalVisibleRect(rectRecommended)
     }
+
+    fun updateSearch(text: String){
+        searchBar.setText(text)
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                val isWithinCustomArea = rectRecommended.contains(event.rawX.toInt(), event.rawY.toInt())
+                Toast.makeText(this, isWithinCustomArea.toString(), Toast.LENGTH_SHORT).show()
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt()) || !isWithinCustomArea) {
+                    v.clearFocus()
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
+
+
+
+    companion object{
+        private lateinit var browseProducts: BrowseProducts
+        fun updateSearch(text:String) {
+            browseProducts.updateSearch(text)
+        }
+    }
+
+
 }
--- frontend/app/src/main/java/com/example/smarttrade/SearchAdapter.kt
+++ frontend/app/src/main/java/com/example/smarttrade/SearchAdapter.kt
@@ -8,44 +8,51 @@ import android.widget.BaseAdapter
 import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.smarttrade.nonactivityclasses.ImageURLtoBitmapConverter
 import com.example.smarttrade.nonactivityclasses.LeafColor
 import com.example.smarttrade.nonactivityclasses.category_representation
 import com.example.smarttrade.nonactivityclasses.product_representation
+import com.example.smarttrade.nonactivityclasses.search_representation
 
 
-class CategoryAdapter(
-    private val categories: MutableList<category_representation>
-) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+class SearchAdapter(
+    private val prevSearch: MutableList<search_representation>
+) : RecyclerView.Adapter<SearchAdapter.CategoryViewHolder>() {
 
     class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         return CategoryViewHolder(
             LayoutInflater.from(parent.context).inflate(
-                R.layout.category_select,
+                R.layout.recommended_search_representation,
                 parent,
                 false
             )
         )
     }
 
-    fun addAllCategories(list:MutableList<category_representation>){
-        for(cat in list){
-            categories.add(cat)
+    fun addAllCategories(list:MutableList<search_representation>){
+        for(s in list){
+            prevSearch.add(s)
         }
         notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int {
-        return categories.size
+        return prevSearch.size
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        val curCat = categories[position]
-        val image = holder.itemView.findViewById<ImageView>(R.id.imageViewCat)
-        image.setImageResource(curCat.image)
+        val curS = prevSearch[position]
+        val textSearch = holder.itemView.findViewById<TextView>(R.id.textViewPreviousSearch)
+        textSearch.text = curS.search
+        val selected = holder.itemView.findViewById<ConstraintLayout>(R.id.layoutSearch)
+
+        selected.setOnClickListener {
+            BrowseProducts.updateSearch(curS.search)
+        }
     }
 
 }
