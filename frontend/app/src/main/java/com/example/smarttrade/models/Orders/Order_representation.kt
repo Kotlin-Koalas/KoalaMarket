a186ba9eede34ef66085dd94e8987c0936dcd5db
Cesar Gimeno Castellote <cesargicas@gmail.com>
2024-05-12 19:53:57 +0200

    patron estrategia preparado

--- frontend/app/src/main/java/com/example/smarttrade/models/Orders/Order_representation.kt
+++ frontend/app/src/main/java/com/example/smarttrade/models/Orders/Order_representation.kt
@@ -1,6 +1,14 @@
 package com.example.smarttrade.models.Orders
 
-class Order_representation () {
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.smarttrade.models.PaymentMethods.PaymentMethod
+import com.example.smarttrade.models.product_representation_cart
+
+class Order_representation (products:MutableList<product_representation_cart>,shippingAddress:String,billingAddress:String,paymentMethod: PaymentMethod,orderID:String,name:String,surname:String){
+
+    lateinit var imageView:ImageView
+    lateinit var textView:TextView
 
     lateinit var shipped:OrderState
     lateinit var delivered:OrderState
@@ -16,9 +24,28 @@ class Order_representation () {
         returned = OrderReturned(this)
     }
 
-    fun setStates(state:OrderState){
-        this.state = state
-        //TODO: set the image of the new state and state name
+    //Metodo para setear la imagen y el texto del estado, llamar antes que nextState().
+    fun setImageView(imageView: ImageView){
+        this.imageView = imageView
     }
 
+    fun setTextView(textView: TextView){
+        this.textView = textView
+    }
+
+    //Metodo a llamar para pasar al siguiente estado.
+    fun nextState(){
+        state.nextState()
+    }
+
+    //Metodo a llamar para realizar la accion del estado, como devolver el pedido.
+    fun stateAction(){
+        state.stateAction()
+    }
+
+    fun setStates(state:OrderState){
+        this.state = state
+        imageView.setImageResource(state.imageResource)
+        textView.text = state.stateName
+    }
 }
---
+++ frontend/app/src/main/java/com/example/smarttrade/models/PaymentMethods/PaymentMethodPaypal.kt
@@ -0,0 +1,41 @@
+package com.example.smarttrade.models.PaymentMethods
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.widget.Button
+import android.widget.TextView
+import com.example.smarttrade.R
+
+class PaymentMethodPaypal(PayPalEmail:String):PaymentMethod {
+    override val customMessage: String = "Estas a punto de pagar con PayPal, usando la siguiente cuenta: $PayPalEmail"
+    override fun showMessage(context: Context) {
+        showCustomDialogBox(context)
+    }
+
+    private fun showCustomDialogBox(context: Context){
+        val dialog = Dialog(context)
+        dialog.setTitle("CONFIRMATION")
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.pop_up_warning)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val messageBox = dialog.findViewById<TextView>(R.id.textViewErrorText)
+        val btnOk = dialog.findViewById<Button>(R.id.buttonOkPopUp)
+        val btnCancel = dialog.findViewById<Button>(R.id.buttonCancelPopUp)
+
+        btnOk.setOnClickListener{
+            //TODO: Implementar la lógica de crear pedido
+        }
+
+        btnCancel.setOnClickListener{
+            dialog.dismiss()
+        }
+
+        messageBox.text = customMessage
+
+        dialog.show()
+
+    }
+}
